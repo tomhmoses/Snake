bcd50d8a031eb1745b9af9aa5add4a7bd092f33e
Tom Moses <tom@tmoses.co.uk>
2022-08-02 23:23:01 +0200

    display board
    need to add style

--- src/game.js
+++ src/game.js
@@ -2,12 +2,43 @@ import React, { useState } from 'react';
 
 export function Game(props) {
     const players = ['X','O']
-    const [turn, setTurn] = useState(players[0])
-    const [board, setBoard] = useState([['','',''],['','',''],['','','']])
+    const [history, setHistory] = useState([[['','',''],['','',''],['','','']]])
+    var turn = players[history.length % players.length]
     return (
         <div>
             <div>Noughts and Crosses</div>
             <div>{turn}'s Turn</div>
+            <Board board={history.at(-1)}/>
+        </div>
+    )
+}
+
+function Board(props) {
+    return (
+        <div>
+            {props.board.map((row, y) => (
+                <Row row={row} y={y}/>
+            ))}
+        </div>
+    )
+}
+
+function Row(props) {
+    return (
+        <div>
+            {props.row.map((element, x) => (
+                <Cell y={props.y} x={x} element={element}/>
+            ))}
+        </div>
+    )
+}
+
+function Cell(props) {
+    // passed current board state
+    // passed an x and y
+    return (
+        <div>
+            {props.element}
         </div>
     )
 }
