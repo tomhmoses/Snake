c65ea71aed282924030c677f4825d5d15d24491b
Tom Moses <tom@tmoses.co.uk>
2022-08-03 11:06:26 +0200

    check for horizontal and vertical winners

--- src/game.js
+++ src/game.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 export function Game(props) {
     const players = ['X','O']
     const [history, setHistory] = useState([[['','',''],['','',''],['','','']]])
+    const [winner, setWinner] = useState('')
     console.log(history)
     var board=history.at(-1)
     var turn = players[history.length % players.length]
@@ -12,17 +13,35 @@ export function Game(props) {
     }
 
     function clickCell(x, y) {
+        if (!winner) {
             var boardCopy = [...board]
             boardCopy[y][x] = turn
             pushToHistory(boardCopy)
+        }
+    }
+
+    if (!winner) {
+        checkForWinner(board, setWinner)
     }
 
     return (
         <div className='bg-black min-h-screen font-semibold text-indigo-400 p-10'>
             <h1 className="text-3xl font-bold underline pb-2">Noughts and Crosses</h1>
-            <div>{turn}'s Turn</div>
+            {!winner && <div>{turn}'s Turn</div>}
+            {winner && <div>{winner} Wins!</div>}
             <Board board={board} clickCell={clickCell}/>
+            <Reset setHistory={setHistory} setWinner={setWinner}/>
         </div>
+    )
+}
+
+function Reset(props) {
+    var reset = () => {
+        props.setHistory([[['','',''],['','',''],['','','']]])
+        props.setWinner('')
+    }
+    return (
+        <div onClick={reset}>Reset</div>
     )
 }
 
@@ -66,4 +85,48 @@ function Cell(props) {
         )
     }
     
+}
+
+function checkForWinner(board, setWinner) {
+    // check horizontal
+    const winAmount = 3;
+    var currentPlayer = ''
+    var currentRun = 0
+    for (var x = 0; x < board.length; x++) {
+        currentPlayer = ''
+        currentRun = 0
+        for (var y = 0; y < board.length; y++) {
+            //check for continuation
+            if (board[y][x] !== '' && board[y][x] === currentPlayer) {
+                currentRun++
+                if (currentRun === winAmount) {
+                    console.log('winner:')
+                    console.log(currentRun)
+                    console.log(currentPlayer)
+                    setWinner(currentPlayer)
+                }
+            } else {//switch to new player
+                currentPlayer = board[y][x]
+                currentRun = 1
+            }
+        }
+    }
+    // check vertical
+    for (var y = 0; y < board.length; y++) {
+        currentPlayer = ''
+        currentRun = 0
+        for (var x = 0; x < board.length; x++) {
+            //check for continuation
+            if (board[y][x] !== '' && board[y][x] === currentPlayer) {
+                currentRun++
+                if (currentRun === winAmount) {
+                    setWinner(currentPlayer)
+                }
+            } else {//switch to new player
+                currentPlayer = board[y][x]
+                currentRun = 1
+            }
+        }
+    }
+    // check diagonal
 }
