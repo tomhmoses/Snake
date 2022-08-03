2798fff3309a051a6290258d84b0fb16d5281ea4
Tom Moses <tom@tmoses.co.uk>
2022-08-03 10:42:01 +0200

    board can be clicked to play

--- src/game.js
+++ src/game.js
@@ -3,12 +3,25 @@ import React, { useState } from 'react';
 export function Game(props) {
     const players = ['X','O']
     const [history, setHistory] = useState([[['','',''],['','',''],['','','']]])
+    console.log(history)
+    var board=history.at(-1)
     var turn = players[history.length % players.length]
+
+    function pushToHistory(board) {
+        setHistory([...history, board ])
+    }
+
+    function clickCell(x, y) {
+            var boardCopy = [...board]
+            boardCopy[y][x] = turn
+            pushToHistory(boardCopy)
+    }
+
     return (
         <div className='bg-black min-h-screen font-semibold text-indigo-400 p-10'>
             <h1 className="text-3xl font-bold underline pb-2">Noughts and Crosses</h1>
             <div>{turn}'s Turn</div>
-            <Board board={history.at(-1)}/>
+            <Board board={board} clickCell={clickCell}/>
         </div>
     )
 }
@@ -17,7 +30,7 @@ function Board(props) {
     return (
         <div className='flex border-2 w-min border-indigo-400'>
             {props.board.map((row, y) => (
-                <Row row={row} y={y}/>
+                <Row row={row} y={y} clickCell={props.clickCell}/>
             ))}
         </div>
     )
@@ -27,18 +40,30 @@ function Row(props) {
     return (
         <div>
             {props.row.map((element, x) => (
-                <Cell y={props.y} x={x} element={element}/>
+                <Cell element={element} x={x} y={props.y} clickCell={props.clickCell}/>
             ))}
         </div>
     )
 }
 
+
+
 function Cell(props) {
-    // passed current board state
-    // passed an x and y
-    return (
-        <div className='border-indigo-100 border-2 h-10 w-10 text-center'>
-            {props.element}
-        </div>
-    )
+    if (props.element) {
+        return (
+            <div className='border-indigo-100 border-2 h-10 w-10 text-center'>
+                {props.element}
+            </div>
+        )
+    } else {
+        var clickThisCell = () => {
+            props.clickCell(props.x,props.y)
+        }
+        return (
+            <div className='border-indigo-100 border-2 h-10 w-10 text-center cursor-pointer' onClick={clickThisCell}>
+                {props.element}
+            </div>
+        )
+    }
+    
 }
