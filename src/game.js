80701f2f2473cd3e270d80876aa7a5c615591e4b
Tom Moses <tom@tmoses.co.uk>
2022-08-04 09:00:09 +0200

    checks diagonals for wins

--- src/game.js
+++ src/game.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export function Game(props) {
     const players = ['X', 'O']
     const [history, setHistory] = useState([[['', '', ''], ['', '', ''], ['', '', '']]])
-    
+
     // console.log(history)
     var lastboard = history.at(-1)
     var board = lastboard.map(function (arr) {
@@ -113,10 +113,12 @@ function checkForWinner(board) {
     const winAmount = 3;
     var currentPlayer = ''
     var currentRun = 0
-    for (var x = 0; x < board.length; x++) {
+    var x
+    var y
+    for (x = 0; x < board.length; x++) {
         currentPlayer = ''
         currentRun = 0
-        for (var y = 0; y < board.length; y++) {
+        for (y = 0; y < board.length; y++) {
             //check for continuation
             if (board[y][x] !== '' && board[y][x] === currentPlayer) {
                 currentRun++
@@ -133,10 +135,10 @@ function checkForWinner(board) {
         }
     }
     // check vertical
-    for (var y = 0; y < board.length; y++) {
+    for (y = 0; y < board.length; y++) {
         currentPlayer = ''
         currentRun = 0
-        for (var x = 0; x < board.length; x++) {
+        for (x = 0; x < board.length; x++) {
             //check for continuation
             if (board[y][x] !== '' && board[y][x] === currentPlayer) {
                 currentRun++
@@ -152,25 +154,81 @@ function checkForWinner(board) {
     // check diagonal down right
     // start bottom left... start only when length possible
     // move to top right... stop when length impossible
-    // var myCalc = (i) => {
-    //     if (i <= board.length) {
-    //         return i
-    //     } else {
-    //         return board.length * 2 - 2 - i
-    //     }
-    // } 
-    // for (var i = winAmount - 1; i < (board.length * 2 - 1 - (winAmount - 1)); y++) { //each diagonal is numbered (starting with 0), heres where to start and end.
-    //     currentPlayer = ''
-    //     currentRun = 0
-    //     for (var n = 0; n < (myCalc(i)); n++) {
-    //         console.log(i)
-    //         console.log(n)
-    //     }
-    // }
+    var calcDiagLength = (i) => { //calculate length of each diagonal
+        if (i < board.length) {
+            return i + 1
+        } else {
+            return board.length * 2 - 1 - i
+        }
+    }
+    var calcDiagStartCoords = (i) => { //returns {x: ?, y: ?}
+        if (i < board.length) {
+            return { x: 0, y: board.length - 1 - i }
+        } else {
+            return { x: i - board.length - 2, y: 0 }
+        }
+    }
+    var i
+    var n
+    var diagStartCoords
+    var diagLength
+    for (i = winAmount - 1; i < (board.length * 2 - 1 - (winAmount - 1)); i++) { //each diagonal is numbered (starting with 0), heres where to start and end.
+        currentPlayer = ''
+        currentRun = 0
+        diagStartCoords = calcDiagStartCoords(i)
+        diagLength = calcDiagLength(i)
+        for (n = 0; n < diagLength; n++) {
+            console.log('-')
+            console.log(diagStartCoords)
+            console.log(n)
+            //check for continuation
+            x = diagStartCoords.x + n
+            y = diagStartCoords.y + n
+            if (board[y][x] !== '' && board[y][x] === currentPlayer) {
+                currentRun++
+                if (currentRun === winAmount) {
+                    return currentPlayer
+                }
+            } else {//switch to new player
+                currentPlayer = board[y][x]
+                currentRun = 1
+            }
+        }
+    }
     // check diagonal up right
     // start top left... 
-    // end top right...
-
+    // end bottom right...
+    var calcDiagUpStartCoords = (i) => { //returns {x: ?, y: ?}
+        if (i < board.length) {
+            return { x: 0, y: i }
+        } else {
+            return { x: i - board.length + 1, y: board.length - 1 }
+        }
+    }
+    console.log('diag/..')
+    for (i = winAmount - 1; i < (board.length * 2 - 1 - (winAmount - 1)); i++) { //each diagonal is numbered (starting with 0), heres where to start and end.
+        currentPlayer = ''
+        currentRun = 0
+        diagStartCoords = calcDiagUpStartCoords(i)
+        diagLength = calcDiagLength(i)
+        for (n = 0; n < diagLength; n++) {
+            console.log('-')
+            console.log(diagStartCoords)
+            console.log(n)
+            //check for continuation
+            x = diagStartCoords.x + n
+            y = diagStartCoords.y - n
+            if (board[y][x] !== '' && board[y][x] === currentPlayer) {
+                currentRun++
+                if (currentRun === winAmount) {
+                    return currentPlayer
+                }
+            } else {//switch to new player
+                currentPlayer = board[y][x]
+                currentRun = 1
+            }
+        }
+    }
 }
 
 function classNames(...classes) {
