81fef888d1df1210241984a6a839dbf8e8386d7d
Tom Moses <tom@tmoses.co.uk>
2022-08-15 18:01:24 +0100

    player list changes

--- src/Players.js
+++ src/Players.js
@@ -6,13 +6,22 @@ import React from 'react';
 
 export function Players(props) {
     // turn, players, winner, user
+    var players = [];
+    for (var key in props.players) {
+        if (props.players.hasOwnProperty(key)) {
+            players.push( {...props.players[key]} );
+        }
+    }
+    players.sort(function(first, second) {
+        return second.playerNum - first.playerNum;
+    });
     
     return (
         <div>
             {/* list of the players in the game */}
             <div className='flex'>
-                {props.players.map((player, index) => (
-                    <div key={index} className='flex'>{player}</div>
+                {players.map((player, index) => (
+                    <div key={index} className='flex'>{player.symvol}</div>
                         ))}
             </div>
         </div>
