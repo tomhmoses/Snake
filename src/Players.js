85351a538919f3128c8340332ea4395157da263a
Tom Moses <tom@tmoses.co.uk>
2022-08-15 18:08:40 +0100

    additional styling

--- src/Players.js
+++ src/Players.js
@@ -1,27 +1,30 @@
 import React from 'react';
 
-// function classNames(...classes) {
-//     return classes.filter(Boolean).join(' ')
-// }
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export function Players(props) {
     // turn, players, winner, user
     var players = [];
     for (var key in props.players) {
         if (props.players.hasOwnProperty(key)) {
-            players.push( {...props.players[key]} );
+            players.push( {...props.players[key], uid: key} );
         }
     }
     players.sort(function(first, second) {
         return second.playerNum - first.playerNum;
     });
+    var turn = props.turn % players.length;
     
     return (
         <div>
-            {/* list of the players in the game */}
             <div className='flex'>
                 {players.map((player, index) => (
-                    <div key={index} className='flex'>{player.symvol}</div>
+                    <div key={index} className={classNames(
+                        turn === index ? 'animate-pulse' : '',
+                        player.uid === props.user.uid ? 'bg-indigo-600' : 'bg-indigo-100',
+                         '')}>{player.symbol}</div>
                         ))}
             </div>
         </div>
