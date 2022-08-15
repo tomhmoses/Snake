b020f0c9b6bbf79a89dd740272048ed351a981df
Tom Moses <tom@tmoses.co.uk>
2022-08-15 21:35:18 +0100

    move functions to europe-west2

--- functions/index.js
+++ functions/index.js
@@ -5,252 +5,239 @@ admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-// exports.addMessage = functions.https.onRequest(async (req, res) => {
-//     // Grab the text parameter.
-//     const original = req.query.text;
-//     // Push the new message into Firestore using the Firebase Admin SDK.
-//     const writeResult = await admin.firestore().collection("messages")
-//         .add({original: original});
-//     // Send back a message that we"ve successfully written the message
-//     res.json({result: `Message with ID: ${writeResult.id} added.`});
-//   });
 
 // Create a new game in Firestore and return the game ID.
-exports.createGame = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  // get uid from idToken
-  const uid = await getUid(req.query.idToken);
-  // check we have a uid
-  if (!uid) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-  // get the game size from the request
-  const size = parseInt(req.query.size);
-  // get the win size from the request
-  const winSize = parseInt(req.query.winSize);
-  // check valid size
-  if (size < 1) {
-    res.status(400).send("Invalid size");
-    return;
-  }
-  // check for valid win size
-  if (winSize > size) {
-    res.status(400).send("winSize must be less than or equal to size");
-    return;
-  }
-  // create the board
-  const board = createBoard(size);
-  console.log("board", board);
-  // create a new game in Firestore using the Firebase Admin SDK
-  const game = {
-    board: board,
-    size: size,
-    winSize: winSize,
-    turn: 0,
-    winner: null,
-    started: false,
-  };
-  game["players"] = {};
-  game["players"][uid] = {symbol: "X", color: "indigo", playerNum: 0};
+exports.createGame = functions
+    .region("europe-west2").https.onRequest(async (req, res) => {
+      res.set("Access-Control-Allow-Origin", "*");
+      // get uid from idToken
+      const uid = await getUid(req.query.idToken);
+      // check we have a uid
+      if (!uid) {
+        res.status(401).send("Unauthorized");
+        return;
+      }
+      // get the game size from the request
+      const size = parseInt(req.query.size);
+      // get the win size from the request
+      const winSize = parseInt(req.query.winSize);
+      // check valid size
+      if (size < 1) {
+        res.status(400).send("Invalid size");
+        return;
+      }
+      // check for valid win size
+      if (winSize > size) {
+        res.status(400).send("winSize must be less than or equal to size");
+        return;
+      }
+      // create the board
+      const board = createBoard(size);
+      console.log("board", board);
+      // create a new game in Firestore using the Firebase Admin SDK
+      const game = {
+        board: board,
+        size: size,
+        winSize: winSize,
+        turn: 0,
+        winner: null,
+        started: false,
+      };
+      game["players"] = {};
+      game["players"][uid] = {symbol: "X", color: "indigo", playerNum: 0};
 
-  // create ID for the game
-  let gameId = "";
-  // create an unused gameId
-  do {
-    gameId = makeId(5);
-  // eslint-disable-next-line max-len
-  } while (await admin.firestore().collection("games").doc(gameId).get().then((doc) => {
-    return doc.exists;
-  }));
-  // add the game to Firestore
-  await admin.firestore().collection("games").doc(gameId).set(game);
-  res.json({
-    result: `Game with ID: ${gameId} created.`,
-    gameId: gameId,
-  });
-});
+      // create ID for the game
+      let gameId = "";
+      // create an unused gameId
+      do {
+        gameId = makeId(5);
+        // eslint-disable-next-line max-len
+      } while (await admin.firestore().collection("games").doc(gameId).get().then((doc) => {
+        return doc.exists;
+      }));
+      // add the game to Firestore
+      await admin.firestore().collection("games").doc(gameId).set(game);
+      res.json({
+        result: `Game with ID: ${gameId} created.`,
+        gameId: gameId,
+      });
+    });
 
 // Join a game
-exports.joinGame = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  // get uid from idToken
-  const uid = await getUid(req.query.idToken);
-  // check we have a uid
-  if (!uid) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-  // get the game ID from the request
-  const gameId = req.query.gameId;
-  // check we were sent a gameId
-  if (!gameId) {
-    res.status(400).send("Game not found.");
-    return;
-  }
-  // get the player"s symbol from the request
-  const symbol = req.query.symbol;
-  // check we were sent a symbol
-  if (!symbol) {
-    res.status(400).send("Invalid Symbol");
-    return;
-  }
-  // get the player"s color from the request
-  const color = req.query.color;
-  // check if the game exists
-  const gameRef = admin.firestore().collection("games").doc(gameId);
-  const gameData = await gameRef.get();
-  if (!gameData.exists) {
-    res.status(404).send("Game not found.");
-    return;
-  }
-  // check if game is started
-  if (gameData.data().started) {
-    res.status(400).send("Game already started.");
-    return;
-  }
-  // check if the player is already in the game
-  const players = gameData.data().players;
-  console.log("players", players);
-  if (players[uid]) {
-    res.status(200).send("Cool! Player already in game.");
-    return;
-  }
-  // check if symbol is already in use
-  const symbols = Object.values(players).map((player) => player.symbol);
-  if (symbols.includes(symbol)) {
-    res.status(400).send("Symbol already in use.");
-    return;
-  }
-  // add the player to the game
-  players[uid] = {
-    symbol: symbol,
-    color: color,
-    playerNum: Object.keys(players).length,
-  };
-  await gameRef.update({players: players});
-  res.json({
-    result: `Cool! Player with UID: ${uid} added to game with ID: ${gameId}.`,
-  });
-});
+exports.joinGame = functions
+
+    .region("europe-west2").https.onRequest(async (req, res) => {
+      res.set("Access-Control-Allow-Origin", "*");
+      // get uid from idToken
+      const uid = await getUid(req.query.idToken);
+      // check we have a uid
+      if (!uid) {
+        res.status(401).send("Unauthorized");
+        return;
+      }
+      // get the game ID from the request
+      const gameId = req.query.gameId;
+      // check we were sent a gameId
+      if (!gameId) {
+        res.status(400).send("Game not found.");
+        return;
+      }
+      // get the player"s symbol from the request
+      const symbol = req.query.symbol;
+      // check we were sent a symbol
+      if (!symbol) {
+        res.status(400).send("Invalid Symbol");
+        return;
+      }
+      // get the player"s color from the request
+      const color = req.query.color;
+      // check if the game exists
+      const gameRef = admin.firestore().collection("games").doc(gameId);
+      const gameData = await gameRef.get();
+      if (!gameData.exists) {
+        res.status(404).send("Game not found.");
+        return;
+      }
+      // check if game is started
+      if (gameData.data().started) {
+        res.status(400).send("Game already started.");
+        return;
+      }
+      // check if the player is already in the game
+      const players = gameData.data().players;
+      console.log("players", players);
+      if (players[uid]) {
+        res.status(200).send("Cool! Player already in game.");
+        return;
+      }
+      // check if symbol is already in use
+      const symbols = Object.values(players).map((player) => player.symbol);
+      if (symbols.includes(symbol)) {
+        res.status(400).send("Symbol already in use.");
+        return;
+      }
+      // add the player to the game
+      players[uid] = {
+        symbol: symbol,
+        color: color,
+        playerNum: Object.keys(players).length,
+      };
+      await gameRef.update({players: players});
+      res.json({
+        result: "Cool! Player added.",
+      });
+    });
 
 // Start a game
-exports.startGame = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  // get uid from idToken
-  const uid = await getUid(req.query.idToken);
-  // check we have a uid
-  if (!uid) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-  // get the game ID from the request
-  const gameId = req.query.gameId;
-  // check if the game exists
-  const gameRef = admin.firestore().collection("games").doc(gameId);
-  const gameData = await gameRef.get();
-  if (!gameData.exists) {
-    res.status(404).send("Game not found.");
-    return;
-  }
-  // check if game is started
-  if (gameData.data().started) {
-    res.status(400).send("Game already started.");
-    return;
-  }
-  // check if there are enough players
-  const players = gameData.data().players;
-  if (Object.keys(players).length < 2) {
-    res.status(400).send("Not enough players.");
-    return;
-  }
-  // check the player is in the game
-  if (!players[uid]) {
-    res.status(400).send("Player not in game.");
-    return;
-  }
-  // start the game
-  await gameRef.update({started: true});
-  res.json({result: `Game with ID: ${gameId} started.`});
-});
+exports.startGame = functions
+    .region("europe-west2").https.onRequest(async (req, res) => {
+      res.set("Access-Control-Allow-Origin", "*");
+      // get uid from idToken
+      const uid = await getUid(req.query.idToken);
+      // check we have a uid
+      if (!uid) {
+        res.status(401).send("Unauthorized");
+        return;
+      }
+      // get the game ID from the request
+      const gameId = req.query.gameId;
+      // check if the game exists
+      const gameRef = admin.firestore().collection("games").doc(gameId);
+      const gameData = await gameRef.get();
+      if (!gameData.exists) {
+        res.status(404).send("Game not found.");
+        return;
+      }
+      // check if game is started
+      if (gameData.data().started) {
+        res.status(400).send("Game already started.");
+        return;
+      }
+      // check if there are enough players
+      const players = gameData.data().players;
+      if (Object.keys(players).length < 2) {
+        res.status(400).send("Not enough players.");
+        return;
+      }
+      // check the player is in the game
+      if (!players[uid]) {
+        res.status(400).send("Player not in game.");
+        return;
+      }
+      // start the game
+      await gameRef.update({started: true});
+      res.json({result: `Game with ID: ${gameId} started.`});
+    });
 
 // Play a turn in a game
-exports.playTurn = functions.https.onRequest(async (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  // get uid from idToken
-  const uid = await getUid(req.query.idToken);
-  // check we have a uid
-  if (!uid) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
-  // get the game ID from the request
-  const gameId = req.query.gameId;
-  // get the player"s turn from the request
-  const x = parseInt(req.query.x);
-  const y = parseInt(req.query.y);
-  // check if the game exists
-  const gameRef = admin.firestore().collection("games").doc(gameId);
-  const gameData = await gameRef.get();
-  if (!gameData.exists) {
-    res.status(404).send("Game not found.");
-    return;
-  }
-  // check if game is started
-  if (!gameData.data().started) {
-    res.status(400).send("Game not started.");
-    return;
-  }
-  // check if game is over
-  if (gameData.data().winner) {
-    res.status(400).send("Game already over.");
-    return;
-  }
-  // check if the player is in the game
-  const players = gameData.data().players;
-  if (!players[uid]) {
-    res.status(400).send("Player not in game.");
-    return;
-  }
-  // check if is the player"s turn
-  const numOfplayers = Object.keys(players).length;
-  console.log("numOfplayers", numOfplayers);
-  console.log("turn", gameData.data().turn % numOfplayers);
-  console.log("playerNum", players[uid].playerNum);
-  if (gameData.data().turn % numOfplayers != players[uid].playerNum) {
-    res.status(400).send("Wrong turn.");
-    return;
-  }
-  // check if the x and y are valid
-  if (x < 0 || x >= gameData.data().size ||
+exports.playTurn = functions
+    .region("europe-west2").https.onRequest(async (req, res) => {
+      res.set("Access-Control-Allow-Origin", "*");
+      // get uid from idToken
+      const uid = await getUid(req.query.idToken);
+      // check we have a uid
+      if (!uid) {
+        res.status(401).send("Unauthorized");
+        return;
+      }
+      // get the game ID from the request
+      const gameId = req.query.gameId;
+      // get the player"s turn from the request
+      const x = parseInt(req.query.x);
+      const y = parseInt(req.query.y);
+      // check if the game exists
+      const gameRef = admin.firestore().collection("games").doc(gameId);
+      const gameData = await gameRef.get();
+      if (!gameData.exists) {
+        res.status(404).send("Game not found.");
+        return;
+      }
+      // check if game is started
+      if (!gameData.data().started) {
+        res.status(400).send("Game not started.");
+        return;
+      }
+      // check if game is over
+      if (gameData.data().winner) {
+        res.status(400).send("Game already over.");
+        return;
+      }
+      // check if the player is in the game
+      const players = gameData.data().players;
+      if (!players[uid]) {
+        res.status(400).send("Player not in game.");
+        return;
+      }
+      // check if is the player"s turn
+      const numOfplayers = Object.keys(players).length;
+      console.log("numOfplayers", numOfplayers);
+      console.log("turn", gameData.data().turn % numOfplayers);
+      console.log("playerNum", players[uid].playerNum);
+      if (gameData.data().turn % numOfplayers != players[uid].playerNum) {
+        res.status(400).send("Wrong turn.");
+        return;
+      }
+      // check if the x and y are valid
+      if (x < 0 || x >= gameData.data().size ||
     y < 0 || y >= gameData.data().size) {
-    res.status(400).send("Invalid x or y.");
-    return;
-  }
-  // check if the x and y are empty
-  const board = gameData.data().board;
-  console.log("board", board);
-  console.log("x", x);
-  console.log("y", y);
-  console.log("whats there", board[y][x]);
-  if (board[y][x] != "_") {
-    res.status(400).send("Space already taken.");
-    return;
-  }
-  // play the turn
-  board[y] = setCharAt(board[y], x, players[uid].symbol);
-  await gameRef.update({board: board, turn: gameData.data().turn + 1});
-  res.json({result: "Turn played."});
-});
+        res.status(400).send("Invalid x or y.");
+        return;
+      }
+      // check if the x and y are empty
+      const board = gameData.data().board;
+      console.log("board", board);
+      console.log("x", x);
+      console.log("y", y);
+      console.log("whats there", board[y][x]);
+      if (board[y][x] != "_") {
+        res.status(400).send("Space already taken.");
+        return;
+      }
+      // play the turn
+      board[y] = setCharAt(board[y], x, players[uid].symbol);
+      await gameRef.update({board: board, turn: gameData.data().turn + 1});
+      res.json({result: "Turn played."});
+    });
 
 /**
  * Set character at a point in a string.
@@ -265,7 +252,8 @@ function setCharAt(str, index, chr) {
 }
 
 // Check for a winner in a game
-exports.checkWinner = functions.firestore.document("/games/{gameId}")
+exports.checkWinner = functions
+    .region("europe-west2").firestore.document("/games/{gameId}")
     .onUpdate((snap, context) => {
       // get the game data
       const gameData = snap.after.data();
@@ -416,28 +404,6 @@ function checkForWinner(board, winSize = 3) {
     }
   }
 }
-
-
-// Listens for new messages added to
-// /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-// exports.makeUppercase =
-// functions.firestore.document("/messages/{documentId}")
-// .onCreate((snap, context) => {
-//   // Grab the current value of what was written to Firestore.
-//   const original = snap.data().original;
-
-//   // Access the parameter `{documentId}` with `context.params`
-//   functions.logger.log("Uppercasing", context.params.documentId, original);
-
-//   const uppercase = original.toUpperCase();
-
-//   // You must return a Promise when performing
-// asynchronous tasks inside a Functions such as
-//   // writing to Firestore.
-//   // Setting an "uppercase" field in Firestore document returns a Promise.
-//   return snap.ref.set({uppercase}, {merge: true});
-// });
 
 // eslint-disable-next-line require-jsdoc
 function createBoard(size) {
